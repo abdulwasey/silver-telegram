cec6546953c67cf4540874a23204ffd7e996febf
Abdul Wasey <abdulwasey13@gmail.com>
2019-06-27 21:35:22 +0530

    fronend

---
+++ silver-telegram-client/src/containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Provider} from "react-redux";
+import {configureStore} from "../store";
+import {BrowserRouter as Router} from "react-router-dom";
+import Navbar from "./Navbar"
+
+const store = configureStore();
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="onboarding">
+         <Navbar />
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
